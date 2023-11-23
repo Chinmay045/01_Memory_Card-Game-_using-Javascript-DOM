4cfe3faf05c40e4c2e180f1bb085004980f547c0
Chinmay <chinz.sandur07@gmail.com>
2023-11-23 14:20:28 +0530

    add logic to flipped cards

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
     this.classList.toggle("flip");
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+    }
+
+    console.log( firstCard,secondCard);
 }
 
 
