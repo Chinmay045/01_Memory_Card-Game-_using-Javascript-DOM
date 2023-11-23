dfebf0d02d9644c0e553f376a6afe49d1297cb37
Chinmay <chinz.sandur07@gmail.com>
2023-11-23 15:15:48 +0530

    add logic to lockBoard

--- index.js
+++ index.js
@@ -2,26 +2,51 @@ const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard() {
+    if(lockBoard)return;
     this.classList.toggle("flip");
 
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
-    } else {
+
+        return;
+    } 
         hasFlippedCard = false;
         secondCard = this;
-    }
-    // console.log(hasFlippedCard);
-    // console.log(firstCard.dataset.framework);
-    // console.log(secondCard.dataset.framework)
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-
-        firstCard.removeEventListener("click", flipCard);
-        secondCard.removeEventListener("click", flipCard)
-    }
+    
+    checkForMatch();
 }
+// console.log(hasFlippedCard);
+// console.log(firstCard.dataset.framework);
+// console.log(secondCard.dataset.framework)
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+    isMatch ? disableCards() : unFlipCards();
+    disableCards();
+
+
+}
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+}
+
+function unFlipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        firstCard.classList.remove('flip');
+    }, 1500);
+
+    lockBoard = false;
+}
+
+
 
 
 
