7d7ca0159c752ff74fef76eb41d66b1f5105e6bd
Chinmay <chinz.sandur07@gmail.com>
2023-11-23 10:16:23 +0530

    add logic to match cards

--- index.js
+++ index.js
@@ -17,8 +17,20 @@ function flipCard() {
 
         // console.log({ firstCard, secondCard });
     }
-    console.log(firstCard.dataset.framework);
-    console.log(secondCard.dataset.framework);
+    // console.log(firstCard.dataset.framework);
+    // console.log(secondCard.dataset.framework);
 
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        firstCard.removeEventListener("click", flipCard);
+        secondCard.removeEventListener("click", flipCard);
+    } else {
+        setTimeout(() => {
+            firstCard.classList.remove("flip")
+            secondCard.classList.remove("flip")
+
+        }, 1500);
+    }
+
+    console.log("Function Executed");
 }
 cards.forEach(card => card.addEventListener("click", flipCard))
