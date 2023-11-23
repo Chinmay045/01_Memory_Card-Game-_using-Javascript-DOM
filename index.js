721e16093d7c173ec1b9111d2ab7c798cd306ae5
Chinmay <chinz.sandur07@gmail.com>
2023-11-23 10:36:31 +0530

    add logic to lock card

--- index.js
+++ index.js
@@ -3,34 +3,52 @@ const cards = document.querySelectorAll(".memory-card");
 
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 
 function flipCard() {
+    if(lockBoard) return;
     this.classList.toggle("flip");
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
+        return
         // console.log({ hasFlippedCard, firstCard });
-    } else {
+    } 
         hasFlippedCard = false;
         secondCard = this;
 
+        checkForMatch();
         // console.log({ firstCard, secondCard });
     }
+
     // console.log(firstCard.dataset.framework);
     // console.log(secondCard.dataset.framework);
 
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    function checkForMatch() {
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            disableCards();
+        } else {
+            unflipCards();
+        }
+
+        console.log("Function Executed");
+    }
+
+    function disableCards() {
         firstCard.removeEventListener("click", flipCard);
         secondCard.removeEventListener("click", flipCard);
-    } else {
+    }
+
+    function unflipCards() {
+        lockBoard = true;
         setTimeout(() => {
             firstCard.classList.remove("flip")
             secondCard.classList.remove("flip")
-
+            lockBoard = false;
         }, 1500);
     }
 
-    console.log("Function Executed");
-}
+
+
 cards.forEach(card => card.addEventListener("click", flipCard))
